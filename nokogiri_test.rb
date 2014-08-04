0137def5c1bbca38138ee7f974fb0577460b3585
Jisu Kim <jisu.kim@flatironschool.com>
2014-08-04 11:44:55 -0400

    Scrape cash back %

--- nokogiri_test.rb
+++ nokogiri_test.rb
@@ -6,6 +6,12 @@ url = "http://www.ebates.com/stores/all/index.htm"
 doc = Nokogiri::HTML(open(url))
 puts doc.at_css("title").text
 
+stores = []
 doc.css("#moreStoresTable .storeName").each do |item|
-  puts item.at_css("a").text
+  stores << item.at_css("a").text
+end
+
+prices = []
+doc.css("#moreStoresTable .storeRebate").each do |item|
+  prices << item.at_css("a").text
 end
