c44fdbf96e42ce4d6fb7ee2edcf2488c4fc2fa4b
Jisu Kim <jisu.kim@flatironschool.com>
2014-08-04 11:21:39 -0400

    Grab store names

--- nokogiri_test.rb
+++ nokogiri_test.rb
@@ -6,3 +6,6 @@ url = "http://www.ebates.com/stores/all/index.htm"
 doc = Nokogiri::HTML(open(url))
 puts doc.at_css("title").text
 
+doc.css("#moreStoresTable .storeName").each do |item|
+  puts item.at_css("a").text
+end
