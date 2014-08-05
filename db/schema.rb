83bbabac5c6e49831a91708568905b75942f624e
Jisu Kim <jisu.kim@flatironschool.com>
2014-08-05 10:57:38 -0400

    Update route

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805032725) do
+ActiveRecord::Schema.define(version: 20140805145328) do
 
   create_table "stores", force: true do |t|
     t.string   "name"
     t.string   "cash_back"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "keywords"
   end
 
 end
