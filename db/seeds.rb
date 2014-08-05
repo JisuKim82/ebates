66b94569aaec5ae21268d377e39ceb7cabb8746d
Jisu Kim <jisu.kim@flatironschool.com>
2014-08-04 23:49:00 -0400

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# require 'rubygems'
+# require 'nokogiri'
+require 'open-uri'
+
+url = "http://www.ebates.com/stores/all/index.htm"
+doc = Nokogiri::HTML(open(url))
+puts doc.at_css("title").text
+
+stores = []
+doc.css("#moreStoresTable .storeName").each do |item|
+  stores << item.at_css("a").text.gsub!(/\s\s+/,' ')
+end
+
+prices = []
+doc.css("#moreStoresTable .storeRebate").each do |item|
+  prices << item.at_css("a").text
+end
+
+#write information to 'stores-cashback.txt'
+# File.open('stores-cashback.txt', 'w') do |file|
+#   (0...stores.length).each do |idx| 
+#     file.puts "#{stores[idx]} #{prices[idx]}"
+#   end 
+# end
+
+(0...stores.length).each do |idx|
+  Store.create!(name: stores[idx], cash_back: prices[idx])
+end
