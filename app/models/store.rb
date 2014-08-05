8fcff901e74e4aa5a71a9440f48bcf2d1b24f878
Jisu Kim <jisu.kim@flatironschool.com>
2014-08-05 10:57:16 -0400

    Add callback to set keywords before save and scope to search for keyword

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,10 @@
 class Store < ActiveRecord::Base
+  before_save :set_keywords
+  
+  scope :search, ->(keyword){where('keywords LIKE ?', "#{keyword.downcase}")}
+
+  protected
+    def set_keywords
+      self.keywords = [name, cash_back].map(&:downcase).join(' ')
+    end
 end
