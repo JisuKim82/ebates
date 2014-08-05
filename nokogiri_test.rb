21b6bd1cc8298c8c23f691de22e9cea6b34657a9
Jisu Kim <jisu.kim@flatironschool.com>
2014-08-04 23:07:56 -0400

    Add data to one line and store in txt file

--- nokogiri_test.rb
+++ nokogiri_test.rb
@@ -8,10 +8,17 @@ puts doc.at_css("title").text
 
 stores = []
 doc.css("#moreStoresTable .storeName").each do |item|
-  stores << item.at_css("a").text
+  stores << item.at_css("a").text.gsub!(/\s\s+/,' ')
 end
 
 prices = []
 doc.css("#moreStoresTable .storeRebate").each do |item|
   prices << item.at_css("a").text
+end
+
+#write information to 'stores-cashback.txt'
+File.open('stores-cashback.txt', 'w') do |file|
+  (0...stores.length).each do |idx| 
+    file.puts "#{stores[idx]} #{prices[idx]}"
+  end 
 end
