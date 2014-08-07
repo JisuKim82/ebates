55895f0ae4cf5d2affe3d75e9038e3b80c161e41
Jackie <chenjackie74@gmail.com>
2014-08-06 22:38:14 -0400

    fixed slogan and bg image

--- app/assets/javascripts/ebates.js
+++ app/assets/javascripts/ebates.js
@@ -15,8 +15,20 @@ $(document).ready(function(){
    $('.collapse').collapse()
    $('.collapse.in').collapse()
    $('.collapsing').collapse()
+
   });
 }); 
+
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
+
 
 
 
