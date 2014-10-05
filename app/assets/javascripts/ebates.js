fef48c3ef973dac290b41988467a991c3359df54
Jisu Kim <jisu.kim@flatironschool.com>
2014-10-04 20:06:24 -0400

    Remove tooltip properties

--- app/assets/javascripts/ebates.js
+++ app/assets/javascripts/ebates.js
@@ -1,33 +1,33 @@
-$(document).ready(function(){
-  $('#example-btn').on('click', function(event){
-    event.preventDefault();
-    event.stopPropagation();
-    $(this).closest('.inner').find('#example').slideToggle('slow');
-  });
+// $(document).ready(function(){
+//   $('#example-btn').on('click', function(event){
+//     event.preventDefault();
+//     event.stopPropagation();
+//     $(this).closest('.inner').find('#example').slideToggle('slow');
+//   });
 
-  $("a#giftcards").tooltip();
+//   $("a#giftcards").tooltip();
 
-  $('a#giftcards').on('click', function(event){
-    event.preventDefault();
-	  event.stopPropagation();
-	  $(this).tooltip();
+//   $('a#giftcards').on('click', function(event){
+//     event.preventDefault();
+// 	  event.stopPropagation();
+// 	  $(this).tooltip();
 
-   $('.collapse').collapse()
-   $('.collapse.in').collapse()
-   $('.collapsing').collapse()
+//    $('.collapse').collapse()
+//    $('.collapse.in').collapse()
+//    $('.collapsing').collapse()
 
-  });
-}); 
+//   });
+// }); 
 
-if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-  var msViewportStyle = document.createElement('style')
-  msViewportStyle.appendChild(
-    document.createTextNode(
-      '@-ms-viewport{width:auto!important}'
-    )
-  )
-  document.querySelector('head').appendChild(msViewportStyle)
-}
+// if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+//   var msViewportStyle = document.createElement('style')
+//   msViewportStyle.appendChild(
+//     document.createTextNode(
+//       '@-ms-viewport{width:auto!important}'
+//     )
+//   )
+//   document.querySelector('head').appendChild(msViewportStyle)
+// }
 
 
 
